47aff51451d78d177abb99232a01ad3970a3d095
Carlos Mori <cmori.94@gmail.com>
2026-02-12 15:12:16 -0300

    fix: initial commit

--- apps/web/.next/server/webpack-runtime.js
+++ apps/web/.next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("87023c9cdeb2e299")
+/******/ 		__webpack_require__.h = () => ("ca7788d4e67e245d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- apps/web/.next/static/chunks/webpack.js
+++ apps/web/.next/static/chunks/webpack.js
@@ -197,7 +197,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "351a174749890965"; }
+/******/ 		__webpack_require__.h = function() { return "5a629a643b46f538"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
